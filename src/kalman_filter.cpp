0b8d918e496bd23afb670a69e7de9ffe52a9cc98
Seunghun Lee <waydi1@gmail.com>
2017-04-30 23:27:03 +0900

    Implement UpdateEKF

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -1,5 +1,7 @@
+#include <iostream>
 #include "kalman_filter.h"
 
+using namespace std;
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
@@ -49,4 +51,32 @@ void KalmanFilter::UpdateEKF(const VectorXd &z) {
   TODO:
     * update the state by using Extended Kalman Filter equations
   */
+
+  VectorXd hx(3);
+  float px = x_(0);
+  float py = x_(1);
+  float vx = x_(2);
+  float vy = x_(3);
+
+  // divided by zero check
+  if (px == 0 && py == 0) {
+    cout << "CalculateJacobian () - Error - Divided by Zero" << endl;
+    return;
+  }
+
+  hx(0) = sqrt(px*px + py*py);
+  hx(1) = atan(py / px);
+  hx(2) = (px*vx + py+vy) / hx(0);
+
+
+  VectorXd y = z - hx;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd I = MatrixXd::Identity(4, 4);
+  MatrixXd K =  P_ * Ht * Si;
+
+  //new state
+  x_ = x_ + (K * y);
+  P_ = (I - (K * H_)) * P_;
 }
